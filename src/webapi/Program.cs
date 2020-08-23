465af0a4bfbafe9c96f25c0fc16643fb5da9f1e9
Victor Vilela <vilela_victor@hotmail.com>
2020-08-23 02:06:25 -0500

    adds serilog traces

--- src/webapi/Program.cs
+++ src/webapi/Program.cs
@@ -1,23 +1,54 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace VVJ.AppWithDataDog.WebApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Console.WriteLine(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
+            Console.WriteLine(Environment.GetEnvironmentVariable("ASPNETCORE_URLS"));
+            Console.WriteLine(Environment.GetEnvironmentVariable("DD_TRACE_AGENT_URL"));
+            Console.WriteLine(Environment.GetEnvironmentVariable("DD_ENV"));
+            Console.WriteLine(Environment.GetEnvironmentVariable("DD_SERVICE"));
+            Console.WriteLine(Environment.GetEnvironmentVariable("DD_VERSION"));
+            Console.WriteLine(Environment.GetEnvironmentVariable("DD_TAGS"));
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration,sectionName: "Serilog")
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- src/webapi/Startup.cs
+++ src/webapi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace VVJ.AppWithDataDog.WebApi
 {
@@ -35,6 +36,8 @@ namespace VVJ.AppWithDataDog.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSerilogRequestLogging();
 
             app.UseHttpsRedirection();
 
